41d837049ec60304b3fc82b8adc643e1ff8d5ae0
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2020-10-14 02:19:17 +0800

    Delete userSettingsListener.ts

--- src/listeners/userSettingsListener.ts
+++
@@ -1,9 +0,0 @@
-import * as request from "../helpers/request";
-import * as listenerList from "./mainListenerCall";
-
-window.addEventListener("click", e => {
-    //request.sendRequest({ navInstruction: ["appearance", "theme"], val: "light"}, "modifySetting");
-    if ((e.target as HTMLElement).closest(".faded")) {
-        listenerList.closePopup();
-    }
-})
