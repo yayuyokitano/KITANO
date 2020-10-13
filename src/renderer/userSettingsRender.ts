54c5db3081f23209c24d4ec9cd097dd749f80420
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2020-10-14 02:50:30 +0800

    Styling and some accessibility

--- src/renderer/userSettingsRender.ts
+++ src/renderer/userSettingsRender.ts
@@ -1,7 +1,7 @@
 import * as request from "../helpers/request";
 
 export function setSettingsPopup(settings:any) {
-    const appearance = `<h2>Appearance</h2><span>Theme: </span><select class="themeSelect" onchange="listeners.changeTheme(this.value)"><option value="light">light</option><option value="dark">dark</option></select>`;
+    const appearance = `<h2>Appearance</h2><span>Theme: </span><select class="themeSelect" onchange="listeners.changeTheme(this.value); this.blur()"><option value="light">light</option><option value="dark">dark</option></select>`;
     (document.querySelector("#popupDetails") as HTMLElement).innerHTML = `${appearance}`;
     (document.querySelector(`.themeSelect option[value="${settings.appearance.theme}"]`) as HTMLElement).setAttribute("selected", "selected");
 }
