23e412ab700a1e615b8941ac5c151e639c94bf03
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2020-10-14 02:18:17 +0800

    Delete userSettingsRender.ts

--- src/callbacks/renderfn/userSettingsRender.ts
+++
@@ -1,7 +0,0 @@
-import * as request from "../../helpers/request";
-
-export function setSettingsPopup(settings:any) {
-    const appearance = `<h2>Appearance</h2><span>Theme: </span><select class="themeSelect" onchange="listeners.changeTheme(this.value)"><option value="light">light</option><option value="dark">dark</option></select>`;
-    (document.querySelector("#popupDetails") as HTMLElement).innerHTML = `${appearance}`;
-    (document.querySelector(`.themeSelect option[value="${settings.appearance.theme}"]`) as HTMLElement).setAttribute("selected", "selected");
-}
